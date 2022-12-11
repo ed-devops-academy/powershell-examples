568360951b0fe1a57b44b3b1e80a4fac675c0a06
Eduardo Miguel Hernández <emiguelc1991@protonmail.com>
2022-12-11 00:55:23 -0500

    Added from exercise one the three and four sections

--- exercises_techskool/exercise1.ps1
+++ exercises_techskool/exercise1.ps1
@@ -14,44 +14,80 @@ function New-SubFolder {
     $subfolderPath = "$testingFolderPath/SubFolder$subfolderIndex"
 
     if (-Not (Test-Path $subfolderPath)) {
-        New-Item -ItemType Directory -Path "$subfolderPath"
+        New-Item -ItemType Directory -Path $subfolderPath
     }
     else {
         Write-Host "$subfolderPath already exists"
     }
-    return $subfolderPath
+}
+function New-File {
+    param (
+        [string][Parameter(Mandatory)]
+        $filePath,
+        [string][Parameter(Mandatory)]
+        $fileName
+    )
+
+    if (-Not (Test-Path "$filePath/$fileName")) {
+        New-Item -ItemType File -Path $filePath -Name $fileName
+    }
+    else {
+        Write-Host "$filePath/$fileName already exists"
+    }
 }
 
 # Solving part 1
 
 $testingPurposePath = "$TestFolderPath/TestingPurpose"
 if (-Not (Test-Path $testingPurposePath)) {
-    New-Item -ItemType Directory -Path "$testingPurposePath"
-}else{
+    New-Item -ItemType Directory -Path $testingPurposePath
+}
+else {
     Write-Host "$testingPurposePath already exists"
 }
 
-$subFoldersPaths = @()
+$subFoldersPaths = [System.Collections.Generic.List[string]]::new()
 
-for ($i = 1; $i -le 3; $i++) {
-    $subFoldersPaths += New-SubFolder -subfolderIndex $i -testingFolderPath $testingPurposePath
+foreach ($value in 1..3) {
+    $subFoldersPaths.Add((New-SubFolder -subfolderIndex $value -testingFolderPath $testingPurposePath))
 }
 
-
-
 # Solving part 2
+$subFoldersPaths
 for ($i = 1; $i -le 100 ; $i++) {
     switch ($i) {
         { $i -ge 1 -and $i -le 50 } { 
             $firstSubfolderPath = $subFoldersPaths[0]
-            $newItemPath = "$firstSubfolderPath/TypeATest$i.txt"
-            if (-Not (Test-Path $newItemPath)) {
-                New-Item -ItemType File -Path $newItemPath
+            New-File -filePath $firstSubfolderPath -fileName "TypeATest$i.txt"
+        }
+        { $i -gt 50 } {
+            $secondSubfolderPath = $subFoldersPaths[1]
+            $subFoldersPaths[1]
+
+            $randomSwitch = Get-Random -Minimum 0 -Maximum 1
+            if ( $randomSwitch -eq 0 ) {
+                New-File -filePath $secondSubfolderPath -fileName "TypeBTest$i.txt"
             }
             else {
-                Write-Host "$newItemPath already exists."
+                New-File -filePath $secondSubfolderPath -fileName "Purpose$i`Test2.txt"
             }
         }
         Default {}
+    }
+}
+
+# 3 and 4 answer
+Get-ChildItem -Path $subFoldersPaths[0], $subFoldersPaths[1]
+| ForEach-Object {
+    $fileName = $_.Name
+    $fileName
+    $pattern = "\w+(?<number>\d+)\w*.txt"
+    $results = [Regex]::Matches($fileName, $Pattern)
+    $number = [int]$results[0].Groups["number"].Value
+    if ( $number % 2 -eq 0 ) {
+        Move-Item -Path $_.FullName -Destination $subFoldersPaths[0]
+    }
+    else {
+        Move-Item -Path $_.FullName -Destination $subFoldersPaths[1]
     }
 }
