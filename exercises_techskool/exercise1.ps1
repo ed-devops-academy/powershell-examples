6bd39fefc3e5669a2662164264594048ecf87542
Eduardo Miguel Hernández <emiguelc1991@protonmail.com>
2022-12-10 20:22:12 -0500

    Better solution to include functions and arrays

--- exercises_techskool/exercise1.ps1
+++ exercises_techskool/exercise1.ps1
@@ -5,23 +5,52 @@ Param(
     [string]$TestFolderPath = './exercises_techskool/output_folder'
 )
 
+function New-SubFolder {
+    param (
+        [Parameter(Mandatory)]
+        $subfolderIndex,
+        $testingFolderPath
+    )
+    $subfolderPath = "$testingFolderPath/SubFolder$subfolderIndex"
+
+    if (-Not (Test-Path $subfolderPath)) {
+        New-Item -ItemType Directory -Path "$subfolderPath"
+    }
+    else {
+        Write-Host "$subfolderPath already exists"
+    }
+    return $subfolderPath
+}
+
 # Solving part 1
+
 $testingPurposePath = "$TestFolderPath/TestingPurpose"
-New-Item -ItemType Directory -Path "$testingPurposePath" 
+if (-Not (Test-Path $testingPurposePath)) {
+    New-Item -ItemType Directory -Path "$testingPurposePath"
+}else{
+    Write-Host "$testingPurposePath already exists"
+}
 
-$subfolder1Path = "$testingPurposePath/SubFolder1"
-$subfolder2Path = "$testingPurposePath/SubFolder2"
-$subfolder3Path = "$testingPurposePath/SubFolder3"
+$subFoldersPaths = @()
 
-New-Item -ItemType Directory -Path "$subfolder1Path" 
-New-Item -ItemType Directory -Path "$subfolder2Path" 
-New-Item -ItemType Directory -Path "$subfolder3Path" 
+for ($i = 1; $i -le 3; $i++) {
+    $subFoldersPaths += New-SubFolder -subfolderIndex $i -testingFolderPath $testingPurposePath
+}
+
+
 
 # Solving part 2
 for ($i = 1; $i -le 100 ; $i++) {
     switch ($i) {
         { $i -ge 1 -and $i -le 50 } { 
-            New-Item -ItemType File -Path "$subfolder1Path/TypeATest$i.txt"
+            $firstSubfolderPath = $subFoldersPaths[0]
+            $newItemPath = "$firstSubfolderPath/TypeATest$i.txt"
+            if (-Not (Test-Path $newItemPath)) {
+                New-Item -ItemType File -Path $newItemPath
+            }
+            else {
+                Write-Host "$newItemPath already exists."
+            }
         }
         Default {}
     }
